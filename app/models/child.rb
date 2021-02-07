fe5cd8114ab789786ff8fc7ccfed16b906dc9016
Sathya Hari <sathyahari9@gmail.com>
2021-02-07 18:34:20 -0500

    lab 5 current status

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,3 +1,11 @@
 class Child < ApplicationRecord
-    
+    has_many :chores
+    has_many :tasks, :through => :chores, :source => :task
+    validates_presence_of :first_name, :last_name
+    scope :alphabetical, -> { order(:last_name, :first_name) }
+    scope :active, -> { where(active: true) }
+
+    def name
+       first_name + " " + last_name
+    end
 end
--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class ChildTest < ActiveSupport::TestCase
   should have_many(:chores)
   should have_many(:tasks).through(:chores)
-  
   should validate_presence_of(:first_name)
   should validate_presence_of(:last_name)
 
